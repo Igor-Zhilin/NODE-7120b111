44748f61871bd7fc7fdd7f5ee1c8e2c24dac2c04
Igor-Zhilin <izilin33@gmail.com>
2024-04-08 21:57:28 +0300

    commit

--- models/entry.js
+++ models/entry.js
@@ -18,3 +18,33 @@ class Entry {
   }
 }
 module.exports = Entry;
+
+// const sqlite3 = require("sqlite3").verbose();
+// const db = new sqlite3.Database("test.db");
+
+// const sql =
+//   "CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, title TEXT, content TEXT NOT NULL)";
+// db.run(sql);
+
+// class Entry {
+//   constructor() {}
+
+//   static async create(data) {
+//     const sql = "INSERT INTO entries (username, title, content) VALUES (?,?,?)";
+//     await db.run(sql, data.username, data.title, data.content);
+//   }
+
+//   static async selectAll() {
+//     return new Promise((resolve, reject) => {
+//       db.all("SELECT * FROM entries", (err, rows) => {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           resolve(rows);
+//         }
+//       });
+//     });
+//   }
+// }
+
+// module.exports = Entry;
--- models/user.js
+++ models/user.js
@@ -44,3 +44,58 @@ class User {
 }
 
 module.exports = User;
+
+// const sqlite3 = require("sqlite3").verbose();
+// const db = new sqlite3.Database("test.db");
+// const bcrypt = require("bcrypt");
+
+// const sql =
+//   "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL, age INTEGER NOT NULL)";
+
+// db.run(sql);
+
+// class User {
+//   constructor() {}
+
+//   static async create(dataForm) {
+//     try {
+//       const salt = await bcrypt.genSalt(10);
+//       const hash = await bcrypt.hash(dataForm.password, salt);
+//       const sql1 =
+//         "INSERT INTO users (name, email, password, age) VALUES (?, ?, ?, ?)";
+//       db.run(sql1, dataForm.name, dataForm.email, hash, dataForm.age);
+//     } catch (err) {
+//       throw err;
+//     }
+//   }
+
+//   static async findByEmail(email) {
+//     return new Promise((resolve, reject) => {
+//       db.get("SELECT * FROM users WHERE email = ?", email, (err, row) => {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           resolve(row);
+//         }
+//       });
+//     })
+//   }
+
+//   static async authenticate(dataForm) {
+//     try {
+//       const user = await User.findByEmail(dataForm.email);
+//       if (!user) return null;
+
+//       const result = await bcrypt.compare(dataForm.password, user.password);
+//       if (result) {
+//         return user;
+//       } else {
+//         return null;
+//       }
+//     } catch (err) {
+//       throw err;
+//     }
+//   }
+// }
+
+// module.exports = User;
